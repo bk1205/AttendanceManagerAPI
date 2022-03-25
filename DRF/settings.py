3c16b855ac13debc8f42054ba823ee0424368df8
Your Name <2000bhaveshkumar@gmail.com>
2022-03-25 16:16:36 +0530

    commit

--- DRF/settings.py
+++ DRF/settings.py
@@ -16,10 +16,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'b0+vb7jqtibt-rv*^ty=t=h!p^u2rfxk00i#5(v_-&gi(9j&w7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
 
 ALLOWED_HOSTS = [
     '10.0.2.2',
